bebe8f0a316efb8e1caa05b6d99069500b7dc549
nadzeyamolchan <85223358+nadzeyamolchan@users.noreply.github.com>
2021-06-07 15:18:29 +0300

    init: create react app pokemon-app (#3)

---
+++ pokemon-app/src/App.js
@@ -0,0 +1,17 @@
+import {Component} from 'react';
+
+class App extends Component{
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+        <div>
+          Some text
+        </div>
+    );
+  }
+}
+
+export default App;
